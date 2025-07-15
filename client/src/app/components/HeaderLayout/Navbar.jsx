f7cee0e80ddaf93be95976f6139ef4e5f754beda
AzKomilov96 <46714958+KomilovDev96@users.noreply.github.com>
2025-07-15 22:34:32 +0500

    new client

--- client/src/app/components/HeaderLayout/Navbar.jsx
+++ client/src/app/components/HeaderLayout/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import styles from './Navbar.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Container } from 'styled-bootstrap-grid';
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
@@ -10,25 +11,28 @@ const Navbar = () => {
 
     return (
         <header className={styles.navbar}>
-            <div className={styles.container}>
-                <div className={styles.logo}>
-                    <Link href="/">
-                        <Image src="/logo.png" alt="NBL Logo" width={100} height={40} />
-                    </Link>
-                </div>
-                <nav className={`${styles.navLinks} ${menuOpen ? styles.open : ''}`}>
-                    <a href="#how">Как играть?</a>
-                    <a href="#winners">Победители</a>
-                    <a href="#ref">Рефералы</a>
-                    <a href="#faq">FAQ</a>
-                </nav>
+            <Container>
+                <div className={styles.container}>
+                    <div className={styles.logo}>
+                        <Link href="/">
+                            <Image src="/logo.png" alt="NBL Logo" width={100} height={40} priority />
+                        </Link>
+                    </div>
+                    <nav className={`${styles.navLinks} ${menuOpen ? styles.open : ''}`}>
+                        <a href="#how">Как играть?</a>
+                        <a href="#winners">Победители</a>
+                        <a href="#ref">Рефералы</a>
+                        <a href="#faq">FAQ</a>
+                    </nav>
 
-                <div className={styles.burger} onClick={toggleMenu}>
-                    <span className={`${styles.line} ${menuOpen ? styles.rotate1 : ''}`} />
-                    <span className={`${styles.line} ${menuOpen ? styles.fadeOut : ''}`} />
-                    <span className={`${styles.line} ${menuOpen ? styles.rotate2 : ''}`} />
+                    <div className={styles.burger} onClick={toggleMenu}>
+                        <span className={`${styles.line} ${menuOpen ? styles.rotate1 : ''}`} />
+                        <span className={`${styles.line} ${menuOpen ? styles.fadeOut : ''}`} />
+                        <span className={`${styles.line} ${menuOpen ? styles.rotate2 : ''}`} />
+                    </div>
                 </div>
-            </div>
+            </Container>
+
         </header>
     );
 };
